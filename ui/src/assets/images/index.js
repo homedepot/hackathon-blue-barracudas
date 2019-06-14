9cea4aaa8165c769302cc3bf35f4988550b4a197
Ninti Chance <ninhursag_chance@homedepot.com>
2019-06-14 13:11:10 -0400

    Add image component and all images we need

---
+++ ui/src/assets/images/index.js
@@ -0,0 +1,14 @@
+export const images ={
+  "blueMonth": require('./blueMonth.png'),
+  "chevronCollapsed": require('./chevronCollapsed.png'),
+  "chevronExpanded": require('./chevronExpanded.png'),
+  "chevronForward": require('./chevronExpanded.png'),
+  "logo": require('./logo.png'),    
+  "logoGalaxy": require('./logoGalaxy.png'),
+  "logoGalaxyColor": require('./logoGalaxyColor.png'),
+  "magentaMonth": require('./magentaMonth.png'),
+  "toBe": require('./toBe.png'),
+  "toGo": require('./toGo.png'),
+  "toMeet": require('./toMeet.png'),
+  "toSee": require('./toSee.png'),
+}
