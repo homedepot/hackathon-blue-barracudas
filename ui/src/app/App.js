940b606be64bdca163c8015a077a582a00d27371
Frank Zondlo <frank_zondlo@homedepot.com>
2019-06-14 16:18:19 -0400

    fixed imports

--- ui/src/app/App.js
+++ ui/src/app/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css'
-import Login from '../Components/login/Login'
-import Landing from '../Components/landing/Landing'
-import Wish from '../Components/wish/Wish'
+import Login from '../components/login/Login'
+import Landing from '../components/landing/Landing'
+import Wish from '../components/wish/Wish'
 import WatchAuth from '../auth/WatchAuth'
 
- import Admin from '../Components/adminLogin/adminLogin'
+ import Admin from '../components/adminLogin/adminLogin'
 
 function App() {
   return (
