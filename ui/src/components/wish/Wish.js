bf4e31ff8b6712ec1fe2e40e8e48c1e3ffcfa1ae
Ninti Chance <ninhursag_chance@homedepot.com>
2019-06-15 22:55:32 +0100

    Change Wish to a list item instead of jumbo container

--- ui/src/components/wish/Wish.js
+++ ui/src/components/wish/Wish.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react"
-import "./Wish.scss"
-import Image from "../image/Image"
+import React, { Component } from 'react'
+import './Wish.scss'
+import Image from '../image/Image'
 
 export default class Wish extends Component {
 
   render() {
     return (
-      <div className="jumbotron wish-container">
-        <div className="profile-picture">
-          <img alt="Profile picture" src={this.props.childImage} />
+      <div className='wish-container'>
+        <div className='profile-picture'>
+          <img className='rounded-image' alt='Profile picture' src={this.props.childImage} />
         </div>
-        <p className="lead"><h3>{this.props.childName}</h3> - Age {this.props.childAge} from {this.props.childTown}</p>
-        <p className="child-details">{this.props.childDetails}</p>    
-        <div className="vertical-line"></div>    
-        <div className="wish information">
-             <div className="sponsor">
-               <img alt="Sponsor" src={`${this.props.sponsor}`} />
+        <p className='lead'><h3>{this.props.childName}</h3> - Age {this.props.childAge} from {this.props.childTown}</p>
+        <p className='child-details'>{this.props.childDetails}</p>    
+        <div className='vertical-line'></div>    
+        <div className='wish information'>
+             <div className='sponsor'>
+               <img alt='Sponsor' src={`${this.props.sponsor}`} />
              </div>
-             <div className="vertical-line"></div>
-             <div className="wish-type icons">
+             <div className='vertical-line'></div>
+             <div className='wish-type icons'>
                <Image
-                 alt="Wish type"
+                 alt='Wish type'
                  source={this.props.wishType}
                />
              </div>
-             <div className="chevron icons">
+             <div className='chevron icons'>
                <Image
-                 alt="Chevron"
-                 source="chevronForward"
+                 alt='Chevron'
+                 source='chevronForward'
                />
              </div>
            </div>
