d22edf7a8410f7e86f42276effc082ed356df328
Ninti Chance <ninhursag_chance@homedepot.com>
2019-06-15 19:12:21 +0100

    Update Wish Component to take in props for child information

--- ui/src/components/wish/Wish.js
+++ ui/src/components/wish/Wish.js
@@ -3,36 +3,37 @@ import './Wish.scss'
 import Image from '../image/Image'
 
 export default class Wish extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className='wish-container'>
-        <div className='child-information'>
-          <div>
-            <img src='https://i.imgur.com/AqQrCXI.jpg?1'/>
+        <div className='child information card'>
+          <div className='profile-picture'>
+            <img src={this.props.childImage} />
           </div>
           <div className='child-info-container'>
-            <div>
-              <span>Child's Name</span> - Age Child's Age from Child's Town
+            <div className='child-name'>
+              <h3>{this.props.childName}</h3> - Age {this.props.childAge} from {this.props.childTown}
             </div>
-            <div>
-              This child is great! He would like to learn TaeKwonDo
+            <div className='child-details'>
+              {this.props.childDetails}
             </div>
           </div>
-        </div>
-
-        <div className='wish-information'>
-          <div className='sponsor'>
-            <Image source='hd-logo'/>
-          </div>
-          <div className='wish-type card'>
-            <Image
-              source='toBe'
-            />
-          </div>
-          <div className='chevron'>
-            <Image
-              source='chevronForward'
-            />
+          <div className='vertical-line'></div>
+          <div className='wish information'>
+            <div className='sponsor'>
+              <img src={`${this.props.sponsor}`} />
+            </div>
+            <div className='vertical-line'></div>
+            <div className='wish-type icons'>
+              <Image
+                source={this.props.wishType}
+              />
+            </div>
+            <div className='chevron icons'>
+              <Image
+                source='chevronForward'
+              />
+            </div>
           </div>
         </div>
       </div>
